63322aa93650322a2f8605b8a3aa984019ceb46a
renkx <m@renkx.com>
2024-01-17 22:12:01 +0800

    update

--- network-reinstall-os.sh
+++ network-reinstall-os.sh
@@ -20,7 +20,7 @@ echo "##############################################################"
 echo "#                                                            #"
 echo "#  Network reinstall OS                                      #"
 echo "#                                                            #"
-echo "#  Last Modified: 2022-08-24                                 #"
+echo "#  Last Modified: 2023-06-12                                 #"
 echo "#  Linux默认密码：IdcOffer.com                               #"
 echo "#  Supported by idcoffer.com                                 #"
 echo "#                                                            #"
@@ -34,20 +34,19 @@ echo "请选择您需要的镜像包:"
 echo "  0) 升级本脚本"
 echo "  1) Debian 10（Buster） 用户名：root 密码：IdcOffer.com"
 echo "  2) Debian 11（Bullseye）用户名：root 密码：IdcOffer.com ,推荐1G内存以上使用"
-echo "  3) CentOS 7 x64 (DD) 用户名：root 密码：Pwd@CentOS"
-echo "  4) CentOS 7 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
-echo "  5) Ubuntu 16.04 LTS (Xenial Xerus) 用户名：root 密码：IdcOffer.com"
-echo "  6) Ubuntu 18.04 LTS (Bionic Beaver) 用户名：root 密码：IdcOffer.com"
-echo "  7) Ubuntu 20.04 LTS (Focal Fossa) 用户名：root 密码：IdcOffer.com ,推荐2G内存以上使用"
-echo "  8) Fedora 32 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
-echo "  9) Fedora 33 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
-echo "  10) Fedora 34 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
-echo "  11) Fedora 35 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
-echo "  12) Fedora 36 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
-echo "  13) RockyLinux 8 (Green Obsidian) 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
-echo "  14) RockyLinux 9 (Blue Onyx) 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
-echo "  15) AlmaLinux 8 （Sky Tiger）用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
-echo "  16) AlmaLinux 9 （Emerald Puma）用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
+echo "  3) Debian 12（Bookworm）用户名：root 密码：IdcOffer.com ,推荐1G内存以上使用"
+echo "  4) Ubuntu 16.04 LTS (Xenial Xerus) 用户名：root 密码：IdcOffer.com"
+echo "  5) Ubuntu 18.04 LTS (Bionic Beaver) 用户名：root 密码：IdcOffer.com"
+echo "  6) Ubuntu 20.04 LTS (Focal Fossa) 用户名：root 密码：IdcOffer.com ,推荐2G内存以上使用"
+echo "  7) Fedora 32 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
+echo "  8) Fedora 33 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
+echo "  9) Fedora 34 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
+echo "  10) Fedora 35 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
+echo "  11) Fedora 36 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
+echo "  12) RockyLinux 8 (Green Obsidian) 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
+echo "  13) RockyLinux 9 (Blue Onyx) 用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
+echo "  14) AlmaLinux 8 （Sky Tiger）用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
+echo "  15) AlmaLinux 9 （Emerald Puma）用户名：root 密码：IdcOffer.com, 要求2G RAM以上才能使用"
 echo "  自定义安装请使用：bash network-reinstall.sh -dd '您的直连'"
 echo ""
 echo -n "请输入编号: "
@@ -56,19 +55,18 @@ case $N in
   0) wget --no-check-certificate -qO network-reinstall-os.sh "https://down.vpsaff.net/linux/dd/network-reinstall-os.sh" && chmod +x network-reinstall-os.sh && wget --no-check-certificate -qO network-reinstall.sh 'https://down.vpsaff.net/linux/dd/network-reinstall.sh' && chmod a+x network-reinstall.sh ;;
   1) bash network-reinstall.sh -d 10 -p IdcOffer.com ;;
   2) bash network-reinstall.sh -d 11 -p IdcOffer.com ;;
-  3) echo "Password: Pwd@CentOS" ; read -s -n1 -p "Press any key to continue..." ; bash network-reinstall.sh -dd 'https://down.vpsaff.net/linux/dd/images/centos-7-image' ;;
-  4) bash network-reinstall.sh -c 7 -p IdcOffer.com ;;
-  5) bash network-reinstall.sh -u 16.04 -p IdcOffer.com ;;
-  6) bash network-reinstall.sh -u 18.04 -p IdcOffer.com ;;
-  7) bash network-reinstall.sh -u 20.04 -p IdcOffer.com ;;
-  8) bash network-reinstall.sh -f 32 -p IdcOffer.com ;;
-  9)  bash network-reinstall.sh -f 33 -p IdcOffer.com ;;
-  10) bash network-reinstall.sh -f 34 -p IdcOffer.com ;;
-  11) bash network-reinstall.sh -f 35 -p IdcOffer.com ;;
-  12) bash network-reinstall.sh -f 36 -p IdcOffer.com ;;
-  13) bash network-reinstall.sh -r 8 -p IdcOffer.com ;;
-  14) bash network-reinstall.sh -r 9 -p IdcOffer.com ;;
-  15) bash network-reinstall.sh -a 8 -p IdcOffer.com ;;
-  16) bash network-reinstall.sh -a 9 -p IdcOffer.com ;;
+  3) bash network-reinstall.sh -d 12 -p IdcOffer.com ;;
+  4) bash network-reinstall.sh -u 16.04 -p IdcOffer.com ;;
+  5) bash network-reinstall.sh -u 18.04 -p IdcOffer.com ;;
+  6) bash network-reinstall.sh -u 20.04 -p IdcOffer.com ;;
+  7) bash network-reinstall.sh -f 32 -p IdcOffer.com ;;
+  8)  bash network-reinstall.sh -f 33 -p IdcOffer.com ;;
+  9) bash network-reinstall.sh -f 34 -p IdcOffer.com ;;
+  10) bash network-reinstall.sh -f 35 -p IdcOffer.com ;;
+  11) bash network-reinstall.sh -f 36 -p IdcOffer.com ;;
+  12) bash network-reinstall.sh -r 8 -p IdcOffer.com ;;
+  13) bash network-reinstall.sh -r 9 -p IdcOffer.com ;;
+  14) bash network-reinstall.sh -a 8 -p IdcOffer.com ;;
+  15) bash network-reinstall.sh -a 9 -p IdcOffer.com ;;
   *) echo "Wrong input!" ;;
 esac
